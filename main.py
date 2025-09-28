5e0301a204d3a96df23e25074992a373e6ce23d7
dilaksh06 <kamalathasandilakshan@gmail.com>
2025-09-28 23:07:21 +0530

    updaed user routes

--- main.py
+++ main.py
@@ -5,14 +5,4 @@ app=FastAPI()
 
 
 
-@app.get("/user/login")
-def login_user():
-    return ("user login")
 
-@app.post("/user/register")
-def register_user():
-    return ("user registraions")
-
-@app.put("/user/update")
-def update_user():
-    return("update user")
---
+++ routers/users.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, Depends, HTTPException, status
+
+app=FastAPI()
+
+@app.get("/user/login")
+def login_user():
+    return ("user login")
+
+@app.post("/user/register")
+def register_user():
+    return ("user registraions")
+
+@app.put("/user/update")
+def update_user():
+    return("update user")
